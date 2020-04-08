e821d3149f3849508a789ba61d8a097992810725
chbigbrother <47962204+chbigbrother@users.noreply.github.com>
2020-04-08 15:12:19 +0900

    Update and rename koa to koa.js

--- koa.js
+++ koa.js
@@ -20,3 +20,14 @@ app.use((ctx, next) => {
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
 });
+
+after you install nodemon 
+$ npm install -g nodemon
+$ nodemon --watch src/ src/koa.js
+and add 
+"scripts": {
+    "start": "node src",
+    "start:dev": "nodemon --watch src/ src/index.js"
+} 
+to the package.json, the server will automatically starts when you make changes.
+The command that you write in the cmd will be yarn start. (In case you want to start it as a developer's mode, write yarn start:dev).
